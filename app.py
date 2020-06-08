7804987c3229f68d59c4ae4afd500f592aa7cd5c
Sanjayini <sanjayini1999@gmail.com>
2020-06-08 13:23:14 -0400

    files added

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-
+'''
 
 from flask import Flask 
 app = Flask(__name__)   
@@ -10,3 +9,21 @@ def sample():
 
 if __name__ == "__main__": 
     app.run()    
+''' 
+
+
+from flask import Flask, request
+from redis import Redis
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return 'Chennai'
+@app.route('/country')
+def get_country():
+    return 'India'
+@app.route('/ip')
+def ip():
+    ip_address = request.remote_addr
+    return ip_address
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
