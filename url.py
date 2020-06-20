fa98655cab91faf003d0f0902a3ec263380e78e7
Sanjayini <sanjayini1999@gmail.com>
2020-06-20 09:54:26 +0530

    files added

--- url.py
+++ url.py
@@ -9,6 +9,13 @@ def hello_admin():
 def hello_guest(guest):
    return 'Hello %s as Guest' % guest   
 
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))   
+
 
 if __name__ == '__main__':
    app.run(debug = True)   
