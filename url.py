d445d4a6ce9aed7ea7c7bc43e757e08c82aa5303
Sanjayini <sanjayini1999@gmail.com>
2020-06-20 08:19:30 +0530

    files added

--- url.py
+++ url.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 @app.route('/admin')
 def hello_admin():
    return 'Hello Admin'
-   
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest   
 
 
 if __name__ == '__main__':
