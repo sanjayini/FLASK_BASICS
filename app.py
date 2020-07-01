9c664adb6526455131cc505e65d6b7db6536b56b
Sanjayini <sanjayini1999@gmail.com>
2020-07-01 08:25:34 +0530

    files added

--- app.py
+++ app.py
@@ -14,16 +14,22 @@ if __name__ == "__main__":
 
 from flask import Flask, request
 from redis import Redis
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return 'Chennai'
+
 @app.route('/country')
 def get_country():
     return 'India'
+
 @app.route('/ip')
 def ip():
     ip_address = request.remote_addr
     return ip_address
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+    
--- request_param.py
+++ request_param.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
-    print request.args['x']
+    print(request.args['x'])
     return "Hello World!"
+
+@app.route("/data/<section>")
+def data(section):
+    assert section == request.view_args['section']
+
 
 if __name__ == "__main__":
     app.run()
